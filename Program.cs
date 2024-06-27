18372840109df737c84baa75bf8c163218cc4fbf
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-06-26 22:23:24 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static int SequentialSearch(int[] arr, int target)
+    {
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] == target)
+            {
+                return i; // Return the index if the target is found
+            }
+        }
+        return -1; // Return -1 if the target is not found
+    }
+
+    static void Main()
+    {
+        int[] arr = { 5, 3, 7, 1, 9, 4 };
+        int target = 7;
+
+        int result = SequentialSearch(arr, target);
+
+        if (result != -1)
+        {
+            Console.WriteLine("Element found at index " + result);
+        }
+        else
+        {
+            Console.WriteLine("Element not found in the list");
+        }
+    }
+}
